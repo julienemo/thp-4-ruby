6d80aaeda6761f8115e012235c7f5c7b7c1b2d21
Julie K <juliette.nada@gmail.com>
2019-04-04 15:20:49 +0200

    NSA login in 3 funcs

---
+++ lib/02_password.rb
@@ -0,0 +1,45 @@
+def vérification
+  puts "Définis un mot de passe"
+  definition = gets.chomp
+  puts "Maintenant saisie ton mot de passe"
+  saisie = gets.chomp
+  while saisie != definition do
+    puts"Mot de passe incorrecte. Resaisie ton mot de passe"
+    saisie = gets.chomp
+  end
+end
+
+def sign_up
+    puts "définis-ton mot de passe."
+    return gets.chomp
+end
+
+
+def welcome_screen
+  puts "-------------"
+  puts "> Bienvenue dans ton espace NSA"
+  puts "> 10,2 millards de dollars sur le compte de"
+  puts "> Harry S. Truman"
+  puts "> Son numéro de compte est"
+  puts "> #{$mdp}"
+
+end
+
+$mdp = sign_up
+
+def log_in
+  $mdp
+  puts "saisie ton mot de passe."
+  saisie = gets.chomp
+  while saisie!=$mdp
+    puts "Mot de passe incorrecte. Resaisie"
+    saisie = gets.chomp
+  end
+end
+
+def perform
+  log_in
+  welcome_screen
+end
+
+perform
